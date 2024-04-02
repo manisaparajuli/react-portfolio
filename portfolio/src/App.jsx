1b2b55426a8b62974f572f9a0c47c9d04a1bb1ee
manisaparajuli <manisaparajuli66@gmail.com>
2024-04-02 18:52:23 +1100

    created a hamburger menu

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,4 +1,5 @@
 import style from './App.module.css';
+import Navbar from './components/Navbar/Navbar';
 
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
 
   return (
     <div className={style.App}>
-      Hey manisha
+      <Navbar/>
     </div>
   )
 }
