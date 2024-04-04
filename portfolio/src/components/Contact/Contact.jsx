564127e7f8d668f476e02a882d55d90fa67bde80
manisaparajuli <manisaparajuli66@gmail.com>
2024-04-04 15:39:22 +1100

    basic style added to footer

--- portfolio/src/components/Contact/Contact.jsx
+++ portfolio/src/components/Contact/Contact.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import {getImageUrl} from '../../utils'
+import styles from './Contact.module.css'
 
 export const Contact = () => {
-  return <footer>
-    <div><h2>Contact</h2>
+  return <footer className={styles.container}>
+    <div className={styles.text}><h2>Contact</h2>
     <p>Feel free to reach out!</p>
     </div>
-    <ul>
-      <li>
+    <ul className={styles.links}>
+      <li className={styles.link}>
         <img src={getImageUrl("contact/emailIcon.png")} alt="email icon" />
         <a href="mailto:manisaparajuli66@gmail.com">manisaparajuli66@gmail.com</a>
       </li>
-      <li>
+      <li className={styles.link}>
         <img src={getImageUrl("contact/githubIcon.png")} alt="github icon" />
         <a href="https://github.com/manisaparajuli" target='_blank'>github/manisaparajuli</a>
       </li>
-      <li>
+      <li className={styles.link}>
         <img src={getImageUrl("contact/linkedinIcon.png")} alt="linkedin icon" />
         <a href="https://www.linkedin.com/in/manishaparajuli/" target='_blank'>LinkedIn.com/manisha</a>
       </li>
