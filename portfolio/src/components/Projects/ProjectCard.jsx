d393fac52805b4de56878cdd8ca912a33dc87317
manisaparajuli <manisaparajuli66@gmail.com>
2024-04-05 18:59:23 +1100

    added styling to projects section

--- portfolio/src/components/Projects/ProjectCard.jsx
+++ portfolio/src/components/Projects/ProjectCard.jsx
@@ -7,17 +7,21 @@ const ProjectCard = ({
 }) => {
 
   return <div className={styles.container}>
-    <img src={getImageUrl(imageSrc)} alt={`image of ${title}`} className={styles.image}/>
-    <h3 className={styles.title}>{title}</h3>
-    <p className={styles.description}>{description}</p>
-    <ul className={styles.skills}>{
-      skills.map((skill, id) => {
-        return (<li key={id} className={styles.skill}>{skill}</li>
-      )})
-      }</ul>
+    
+      <img src={getImageUrl(imageSrc)} alt={`image of ${title}`} className={styles.image}/>
+    
+    <div>
+      <h3 className={styles.title}>{title}</h3>
+      <p className={styles.description}>{description}</p>
+      <ul className={styles.skills}>{
+        skills.map((skill, id) => {
+          return (<li key={id} className={styles.skill}>{skill}</li>
+        )})
+        }</ul>
       <div className={styles.links}>
         <a href={demo} className={styles.link}>Demo</a>
         <a href={source} className={styles.link}>Source</a>
+      </div>
       </div>
   </div>
 }
