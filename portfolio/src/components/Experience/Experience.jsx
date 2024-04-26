86d98549de19bdcc7e56d1365b60fbc53527aec1
manisaparajuli <manisaparajuli66@gmail.com>
2024-04-26 16:03:13 +1000

    color theme changed to white

--- portfolio/src/components/Experience/Experience.jsx
+++ portfolio/src/components/Experience/Experience.jsx
@@ -8,10 +8,12 @@ export const Experience = () => {
   return <section className={styles.container} id="experience">
     <h2 className={styles.title}>Skills & Experience </h2>
     <div className={styles.content}>
+      <h3>Technologies I am familiar with:</h3>
       <div className={styles.skills}>{
         skills.map((skill, id) => {
           return <div key={id} className={styles.skill}>
             <div className={styles.skillImageContainer}>
+
               <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
             </div>
               <p>{skill.title}</p>
@@ -24,11 +26,9 @@ export const Experience = () => {
             history.map((historyItem, id)=>{
               return <li key={id} className={styles.historyItem}>
                 <img src={getImageUrl(historyItem.imageSrc)} alt={`${historyItem.organization} Logo`} />
-
                 <div className={styles.historyItemDetails}> 
                   <h3>{`${historyItem.role}, ${historyItem.organization}`}</h3>
                   <p>{`${historyItem.startDate}- ${historyItem.endDate}`}</p>
-
                   <ul>{historyItem.experiences.map((experience, id)=>{
                     return <li key={id}>{experience}</li>
                   })}
