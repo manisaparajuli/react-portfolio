8f49e906d39c920bb1ed9a529ad44f3749aebbb9
manisaparajuli <manisaparajuli66@gmail.com>
2024-04-26 17:04:20 +1000

    content added to about me section

--- portfolio/src/components/About/About.jsx
+++ portfolio/src/components/About/About.jsx
@@ -11,22 +11,22 @@ export const About = () => {
       <ul className={styles.aboutItems}>
         <li className={styles.aboutItem}>
           <img src={getImageUrl("about/cursorIcon.png")} alt="cursor icon" />
-          <div className={styles.aboutItemText}><h3>My highest qualification</h3>
-            <p>I have completed my master's degree in technology, with a specialization in software engineering.</p>
+          <div className={styles.aboutItemText}><h3>Passion for Front-End Development</h3>
+            <p>What drives me in front-end development is the ability to create intuitive, user-centric interfaces that not only look great but also provide seamless functionality. I'm fascinated by the ever-evolving nature of web development and am always eager to explore new tools and frameworks to enhance my craft.</p>
           </div>
         </li>
 
         <li className={styles.aboutItem}>
           <img src={getImageUrl("about/serverIcon.png")} alt="server icon" />
           <div className={styles.aboutItemText}><h3>Full-stack developer</h3>
-            <p>Although I enjoy front-end development more than anything, I am also proficient in backend development.</p>
+            <p>I'm also deeply interested in backend development. Exploring server-side technologies, databases, and APIs excites me as much as crafting front-end experiences. My goal is to master both domains to create comprehensive, high-quality web applications.</p>
           </div>
         </li>
         
         <li className={styles.aboutItem}>
           <img src={getImageUrl("about/uiIcon.png")} alt="cursor icon" />
-          <div className={styles.aboutItemText}><h3>UI/UX designer</h3>
-            <p>I am a google certified UI/UX designer with strong portfolio</p>
+          <div className={styles.aboutItemText}><h3>Beyond the Code</h3>
+            <p>When I'm not coding, you can often find me on a hiking trail, embracing the adventure and tranquility of nature. I'm passionate about exploring the outdoors and seeking new challenges.</p>
           </div>
         </li>
       </ul>
